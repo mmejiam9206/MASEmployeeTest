ae9a575c675925d031ead9afc62919e234473a85
Miguel Mejia <mmejiam9206@outlook.com>
2018-04-04 01:29:25 -0500

    MASEmployeeApplication - Deleted files and changed routing to show only employee details page

--- MASEmployeeApplication/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace MASEmployeeApplication.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
--- MASEmployeeApplication/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace MASEmployeeApplication.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
